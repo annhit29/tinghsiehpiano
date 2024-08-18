12e38e3470836c99464c543d0af0fced6965d984
annhit29 <ann20010929@gmail.com>
2024-08-19 00:22:18 +0200

    change siteUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+// Define the site URL
+// export const siteUrl = 'https://wtinghsiehpiano.com'; 
+
+// Set siteUrl dynamically based on environment
+const isProduction = process.env.NODE_ENV === 'production';
+const siteUrl = isProduction ? 'https://wtinghsiehpiano.com' : process.env.URL || 'http://localhost:4321/';
+
+export { siteUrl };
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()]

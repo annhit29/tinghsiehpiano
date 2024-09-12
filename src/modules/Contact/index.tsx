8bb7506ee856506458357137d90e792895955ff6
annhit29 <ann20010929@gmail.com>
2024-09-12 22:01:59 +0200

    contact.astro page in two langs, not .astro files: try later to change langs

--- src/modules/Contact/index.tsx
+++ src/modules/Contact/index.tsx
@@ -2,10 +2,19 @@ import { type FC } from "react";
 import * as S from "./styled";
 import { TextBox } from "@components/textBox";
 import { FadeIn } from "@utils/animations/FadeIn";
+// import i18next, { t, changeLanguage } from "i18next"; // Import i18next for translations
+
+// changeLanguage("en");
+
+
+// import { useTranslation } from 'react-i18next'; // Import translation hook
 
 // Contact component is for contact.astro
 
 export const Contact: FC = () => {
+
+    // const { t } = useTranslation(); // Initialize translation
+
     return (
         <S.ContactStyled>
             <S.ContainerStyled>
@@ -19,17 +28,18 @@ export const Contact: FC = () => {
                             // href="mailto:youremail@gmail.com"
                             target="_blank"
                         >
-                            <h3>ting61141@gmail.com</h3>
+                            <h3>Email</h3>
                             <p>
-                                Write me an email if you have any questions
+                                ting61141@gmail.com
                             </p>
                         </TextBox>
                     </FadeIn>
                     <FadeIn delay={0.2}>
                     <TextBox variant="background-text" bgText="Address">
+                        {/* <h3>{t("contact.index.address.title")}</h3> */}
                         <h3>Address</h3>
-                        
-                        {/* <p>Some street 123, 12345 City, Country</p> */}
+
+                        {/* <p>{t("contact.index.address.paragraph")}</p> */}
                         <p>Office: No. 134, Ho-ping East Rd., Sec. 2, Daan District, Taipei City 10671, Taiwan</p>
                     </TextBox>
                     </FadeIn>

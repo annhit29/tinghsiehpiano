a9a3e33a5e4e8ec41d1043fff7e554226856f3c6
annhit29 <ann20010929@gmail.com>
2026-05-18 17:06:48 +0200

    adjust event

--- src/modules/Calendar/index.tsx
+++ src/modules/Calendar/index.tsx
@@ -55,7 +55,7 @@ const events: CalendarEvent[] = [
         title: "2026 Asian Aegean International Piano Master Classes & Music Camp",
         date: "2026-08-06",
         displayDate: "August 3 - 6, 2026",
-        venue: "Department of Music, Song Yi Hall, Soochow University",
+        venue: "Department of Music, Song-Yi Hall, Soochow University",
         description: "",
         href: "https://www.facebook.com/permalink.php?story_fbid=pfbid02gnwXrLh1nQfQxMVqF7GRrtoJubjD37uyCGjVwvdVtoJKevehSDW9pH1cAZJsFf53l&id=100057448209206",
         bgText: "Schedule3",
